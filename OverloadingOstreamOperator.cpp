a770f1a128ed1b7ac8bb5429f9deb47a03539afd
Mourad ouchane <mouradouchane@gmail.com>
2021-04-10 09:38:37 +0100

    new solution :Overloading Ostream Operator )

---
+++ OverloadingOstreamOperator.cpp
@@ -0,0 +1,12 @@
+
+
+// problem : https://www.hackerrank.com/challenges/overloading-ostream-operator
+// solution ===============================================================
+/*
+std::ostream& operator <<(std::ostream& is, Person& person) {
+
+    is << "first_name=" << person.get_first_name() << ",last_name=" << person.get_last_name();
+    return is;
+}
+*/
+// =========================================================================
