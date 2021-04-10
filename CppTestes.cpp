5d248d16cab2b34867534baccbebfed0136e89bb
Mourad ouchane <mouradouchane@gmail.com>
2021-04-10 05:24:40 +0100

    making progress in attr parse soultion not completed yet *_*

--- CppTestes.cpp
+++ CppTestes.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+//#include <vector>
 #include <map>
 #include <string>
 #include <regex>
@@ -9,24 +9,18 @@ namespace REG {
     std::regex reg_endTagName("<\\/(\\w{1,})\\>");
     std::regex reg_values("[\\'|\"](\\w{1,})[\\'|\"]");
     std::regex reg_attr("(\\w{1,})\\s{0,}?=");
-
-    std::regex reg_firstPath("(\\w{0,})\\.");
-    std::regex reg_dotPath("\\.(\\w{0,})");
-    std::regex reg_attrAsk("~(\\w{0,})");
+    std::regex reg_pathFinder("(\\w+)\\.");
+    std::regex reg_targetElementName("\\.?(\\w+)\\~");
 };
 
 class HRML_ELEMENT {
-private:
-    std::string sourceCode;
-    std::string tagName;
-
-    std::map<std::string, std::string> attrs;
-    //std::map<std::string, HRML_ELEMENT*> childs;
-    HRML_ELEMENT* child;
-
 public:
+    std::string tagName;
+    std::map<std::string, std::string> attrs;
+    std::map<std::string, HRML_ELEMENT*> children;
     bool hasChilds = false;
     bool hasEndTage = false;
+
     HRML_ELEMENT(std::string tagname, std::map<std::string, std::string> atrs) :tagName(tagname), attrs(atrs) {}
     HRML_ELEMENT() {}
     ~HRML_ELEMENT() {}
@@ -41,55 +35,71 @@ public:
         attrs = attrsMap;
     }
 
-    void printName() {
-        std::cout << "NAME => " << tagName << std::endl;
-    }
-
-    void printAttrs() {
-        for (auto& atr : attrs) {
-            std::cout << "  " << atr.first << " : " << atr.second << std::endl;
-        }
-    }
-
-    void appendChild(HRML_ELEMENT& childElement) {
-        child = &childElement;
-    }
 };
 
-class line {
-public:
-    std::vector<std::string> path;
-    std::vector<std::string> RequestedAttrs;
-    line() {
-
-    }
-    ~line() {
-
-    }
-};
 
 int main() {
+    std::string arrtest[16] = {
+    "<tag1 v1 = \"123\" v2 = \"43.4\" v3 = \"hello\">", "</tag1>",
 
-    int Queries = 4 , lines = 3;
-    //std::cin >> Queries >> lines;
+    "<tag2 v4 = \"v2\" name = \"Tag2\">",
+        "<tag3 v1 = \"Hello\" v2 = \"World!\">","</tag3>",
+        "<tag4 v1 = \"Hello\" v2 = \"Universe!\">","</tag4>",
+    "</tag2>",
 
-    std::vector<HRML_ELEMENT> elements;
-    HRML_ELEMENT tempElement;
+    "<tag5>",
+        "<tag7 new_val = \"New\">","</tag7>",
+    "</tag5>",
+
+    "<tag6>",
+        "<tag8 intval = \"34\" floatval = \"9.845\">","</tag8>",
+    "</tag6>",
+    };
+
+    std::string arrlines[14] = {
+        "tag1~v1",
+        "tag1~v2",
+        "tag1~v3",
+        "tag4~v2",
+        "tag2.tag4~v1",
+        "tag2.tag4~v2",
+        "tag2.tag3~v2",
+        "tag5.tag7~new_val",
+        "tag5~new_val",
+        "tag7~new_val",
+        "tag6.tag8~intval",
+        "tag6.tag8~floatval",
+        "tag6.tag8~val",
+        "tag8~intval"
+    };
+
+    int Queries = sizeof(arrtest) / sizeof(std::string) , lines = sizeof(arrlines) / sizeof(std::string); //std::cin >> Queries >> lines;
+    int i = 0;
+
+    std::string full_ElementsPath = "";
+    std::map<std::string, HRML_ELEMENT> elements;
+
     std::smatch match;
 
-    std::string strs[] = {"<tag1 value = 'HelloWorld'>","<tag2 name = 'Name1'>","</tag2>","</tag1>"};
+
     // processing whole source code line by line  
-    while (Queries >= 1) {
-        std::string tempSourceCode = strs[Queries-1]; //std::getline(std::cin, tempSourceCode);
+    for (i = 0; i < Queries; i += 1) {
+        HRML_ELEMENT tempElement;
+        std::string tempSourceCode = arrtest[i]; //std::getline(std::cin, tempSourceCode);
 
         // check if string a end tag
         if (std::regex_search(tempSourceCode, match, REG::reg_endTagName)) {
-
+            // need to end element tag here when you switch elments vector to map 
+            elements[match.str(1)].hasEndTage = true;
         }
         else {
-           
+
             // get tag name
-            if (std::regex_search(tempSourceCode, match, REG::reg_tagName)) tempElement.setTagName(match.str(1));
+            if (std::regex_search(tempSourceCode, match, REG::reg_tagName)) {
+                tempElement.setTagName(match.str(1));
+                full_ElementsPath += match.str(1);
+                full_ElementsPath += ".";
+            }
 
             // get attrs & values
             std::sregex_iterator itr_attr(tempSourceCode.begin(), tempSourceCode.end(), REG::reg_attr);
@@ -97,65 +107,82 @@ int main() {
             std::sregex_iterator itr_end;
 
             std::map<std::string, std::string> tempAttrs;
-            while (itr_attr != itr_end) {
+            while (itr_attr != itr_end && itr_vals != itr_end) {
                 tempAttrs.insert({ itr_attr->str(1) , itr_vals->str(1) });
                 itr_attr++;
                 itr_vals++;
             }
             tempElement.setAttrs(tempAttrs);
 
-            // check if there's end tag
-            if (std::regex_search(tempSourceCode, match, REG::reg_endTagName)) {
-                tempElement.hasEndTage = true;
-            }
-
             // push to vector of HRML element  
-            elements.push_back(tempElement);
+            elements.insert({ tempElement.getTagName() , tempElement });
 
-            //reffering to parent element 
-            if (elements.size() > 1) {
-                elements.at(Queries - 1).appendChild(elements.at(Queries));
+            // reffering to parent
+
+            for (std::map <std::string, HRML_ELEMENT>::iterator it = elements.begin(); it != elements.end(); it++) {
+                if (it->second.hasEndTage == false) {
+                    it->second.children.insert({ tempElement.getTagName() , &tempElement });
+                    break;
+                }
             }
+
         }
-
-        Queries -= 1;
-    }
-
-    std::string linesSearch[] = { "tag1.tag2~name","tag1~name","tag1~value" };
-
-    // processing requestes
-    while (lines >= 1) {
-        line tempLine;
-        std::string tempstr = linesSearch[lines-1];
-        //std::getline(std::cin, tempstr);
-
-        // main idea !!! => iterate over line & search depend . or ~
-        std::sregex_iterator it(tempstr.begin(),tempstr.end(),REG::reg_dotPath);
-        std::sregex_iterator end;
-
-        // get first element in path
-        std::regex_search(tempstr, match, REG::reg_firstPath);
-        tempLine.path.push_back(match.str(1));
-
-        // get all next elements in path one by one
-        while (it != end) {
-            tempLine.path.push_back(it->str(1)); 
-            it++;
-        }
-
-        std::sregex_iterator itattr(tempstr.begin(), tempstr.end(), REG::reg_attrAsk);
-        std::sregex_iterator attrend;
-        while (itattr != attrend) {
-            tempLine.RequestedAttrs.push_back(itattr->str(1));
-            itattr++;
-        }
-        
-        std::cout << tempLine.path.size() << " \n " << tempLine.RequestedAttrs.size();
-
-        lines -= 1;
     }
 
 
 
+
+    for (std::map<std::string, HRML_ELEMENT>::iterator element = elements.begin(); element != elements.end(); element++) {
+        std::cout << element->first << std::endl;
+
+        for (std::map<std::string, HRML_ELEMENT*>::iterator subelement = element->second.children.begin(); subelement != element->second.children.end(); subelement++) {
+            std::cout << "      " << subelement->first << std::endl;
+        }
+    }
+
+
+
+
+/*
+    // processing lines "requestes"
+    for (int i = 0; i < lines; i += 1) {
+        std::string tempstr = arrlines[i] ; //std::getline(std::cin, tempstr);
+
+        // get path 
+        std::string temppath = tempstr.substr(0, tempstr.find("~"));
+
+        // get attrbuit
+        std::string tempattr = tempstr.substr(tempstr.find("~") + 1, tempstr.size());
+
+        // check if "temp request path" in compatible with full_ElementsPath
+        if (full_ElementsPath.find(temppath) != std::string::npos) {
+
+            // get target element 
+            std::string targetElementName;
+
+            //check if "temp path" has .  , that mean there's more than one element
+            if (temppath.find(".") != std::string::npos) {
+                targetElementName = temppath.substr(temppath.rfind(".") + 1, temppath.size() - 1);
+            }
+            else { // if not that's mean "temp path" include only one element  ex => tag1~attr
+                targetElementName = temppath;
+            }
+
+            // now after all that we need to access "target element" in "element map" by it's name
+            // and accessing request attrubites map inside "target element" by using "tempattr"
+
+            if (elements[targetElementName].attrs[tempattr] == "") {
+                std::cout << "Not Found!" << std::endl;
+            }
+            else {
+                std::cout << elements[targetElementName].attrs[tempattr] << std::endl;
+            }
+
+        }
+        else { // if not , that mean "temp request path" is an invalid path 
+            std::cout << "Not Found!" << std::endl;
+        }
+    }
+    */
     return 0;
 }
--- attribute Parser.cpp
+++ attribute Parser.cpp
@@ -1,147 +1,190 @@
-/*
-<name>   <(\\w{1,})
-</name>  (<\\/\\s{0,}?\\w{1,}\\>)
-=values  [\\'|\"](\\w{1,})[\\'|\"]
-attr=    (\\w{1,})\\s{0,}?=
-<(\\w{ 1, }) | (\\w{ 1, })\\s{ 0, } ? = | [\\'|\"](\\w{1,})[\\' | \"]|(<\\/\\s{0,}?\\w{1,}\\>)
-*/
+#include <iostream>
+//#include <vector>
+#include <map>
+#include <string>
+#include <regex>
 
-/*
- parser code
- try {
-        std::regex re("<(\\w{1,})|(\\w{1,})\\s{0,}?=|[\\'|\"](\\w{1,})[\\'|\"]|(<\\/\\s{0,}?\\w{1,}\\>)");
+namespace REG {
+    std::regex reg_tagName("<(\\w{1,})");
+    std::regex reg_endTagName("<\\/(\\w{1,})\\>");
+    std::regex reg_values("[\\'|\"](\\w{1,})[\\'|\"]");
+    std::regex reg_attr("(\\w{1,})\\s{0,}?=");
+    std::regex reg_pathFinder("(\\w+)\\.");
+    std::regex reg_targetElementName("\\.?(\\w+)\\~");
+};
 
-        std::sregex_iterator next(subject.begin(), subject.end(), re);
-        std::sregex_iterator end;
-
-        std::smatch match;
-        while (next != end) {
-            match = *next;
-            std::cout << match.str(1) << "\n";
-            next++;
-        }
-
-        std::cout << match.size() << "\n";
-        std::cout << match.length() << "\n";
-
-    }
-    catch (std::regex_error& e) {
-        // Syntax error in the regular expression
-    }
-*/
-
-/*
-class elements {
+class HRML_ELEMENT {
 public:
-     elements(std::string srcCode):sourceCode(srcCode){
-         if (sourceCode.find("</")) isParent = false;
-         // processe & get all attrs
-     }
-    ~elements(){}
-    std::string name;
-    std::string sourceCode;
-    std::vector<std::string> attrs;
-    std::vector<elements *> childs;
-    int q;
-    bool isParent = false;
+    std::string tagName;
+    std::map<std::string, std::string> attrs;
+    std::map<std::string, HRML_ELEMENT*> children;
+    bool hasChilds = false;
+    bool hasEndTage = false;
 
-    bool isThereAttr(std::string attr) {
+    HRML_ELEMENT(std::string tagname, std::map<std::string, std::string> atrs) :tagName(tagname), attrs(atrs) {}
+    HRML_ELEMENT() {}
+    ~HRML_ELEMENT() {}
 
-
+    std::string getTagName() {
+        return tagName;
+    }
+    void setTagName(std::string str) {
+        tagName = str;
+    }
+    void setAttrs(std::map<std::string, std::string> attrsMap) {
+        attrs = attrsMap;
     }
 
-    std::string isThereChild() {
-        if (!isParent) return "Not Found!";
-    }
 };
-
-class HRMLELEMENT {
-    protected:
-        std::vector<elements> sources;
-        std::vector<std::string> queries;
-        int q;
-    public:
-        HRMLELEMENT(){ }
-
-        void setSource(std::string HRMLsrc){
-            elements temp(HRMLsrc); sources.push_back(temp);
-            return;
-        };
-
-        void setQuery(std::string Query) {
-            queries.push_back(Query);
-            return;
-        };
-
-        void printSources() {
-            q = 0;
-            while (q < sources.size()) {
-                std::cout <<"Source " << q << " : " << sources.at(q) << std::endl;
-                q += 1;
-            }
-
-            return;
-        };
-
-        void printQueries() {
-            q = 0;
-            while (q < queries.size()) {
-                std::cout <<"Query " << q <<" : "<< queries.at(q) << std::endl;
-                q += 1;
-            }
-
-            return;
-        }
-
-        ~HRMLELEMENT(){ }
-};
-*/
 
 /*
-  HRMLELEMENT HRML;
-  int N, Q , i = 0;
-  std::cin >> N >> Q;
+int main() {
+    std::string arrtest[16] = {
+    "<tag1 v1 = \"123\" v2 = \"43.4\" v3 = \"hello\">", "</tag1>",
 
-  std::string HRMLstring;
-  std::string HRMLqueries;
+    "<tag2 v4 = \"v2\" name = \"Tag2\">",
+        "<tag3 v1 = \"Hello\" v2 = \"World!\">","</tag3>",
+        "<tag4 v1 = \"Hello\" v2 = \"Universe!\">","</tag4>",
+    "</tag2>",
 
-  // cin new lines of HRML
-  while(i <= N) {
-      std::getline(std::cin,HRMLstring);
-      HRML.setSource(HRMLstring);
-      i += 1;
-  }
-  i = 1;
+    "<tag5>",
+        "<tag7 new_val = \"New\">","</tag7>",
+    "</tag5>",
 
-  HRML.printSources();
+    "<tag6>",
+        "<tag8 intval = \"34\" floatval = \"9.845\">","</tag8>",
+    "</tag6>",
+    };
 
-  std::cout << "queries time :)" << std::endl;
-  // cin all queries
-  while (i <= Q) {
-      std::getline(std::cin, HRMLqueries);
-      HRML.setQuery(HRMLqueries);
-      i += 1;
-  }
+    std::string arrlines[14] = {
+        "tag1~v1",
+        "tag1~v2",
+        "tag1~v3",
+        "tag4~v2",
+        "tag2.tag4~v1",
+        "tag2.tag4~v2",
+        "tag2.tag3~v2",
+        "tag5.tag7~new_val",
+        "tag5~new_val",
+        "tag7~new_val",
+        "tag6.tag8~intval",
+        "tag6.tag8~floatval",
+        "tag6.tag8~val",
+        "tag8~intval"
+    };
 
-  HRML.printQueries();
-  */
+    int Queries = sizeof(arrtest) / sizeof(std::string), lines = sizeof(arrlines) / sizeof(std::string); //std::cin >> Queries >> lines;
+    int i = 0;
 
-  /*
-  std::regex reg("<(\\w{1,})");
-  std::smatch smatch;
+    std::string full_ElementsPath = "";
+    std::map<std::string, HRML_ELEMENT> elements;
 
-  std::string str = "<tag1 value=\"value\" id='tr' src=\"node\"> </tag1>";
+    std::smatch match;
 
-  std::sregex_iterator next(str.begin(), str.end(), reg);
-  std::sregex_iterator end;
 
-  while (next != end) {
-      smatch = *next;
+    // processing whole source code line by line  
+    for (i = 0; i < Queries; i += 1) {
+        HRML_ELEMENT tempElement;
+        std::string tempSourceCode = arrtest[i]; //std::getline(std::cin, tempSourceCode);
 
-      std::cout <<"name  : "<< smatch.str(1) << "\n";
-      std::cout <<"attr  : " << smatch.str(2) << "\n";
-      std::cout <<"value : " << smatch.str(3) << "\n";
-      std::cout <<"/end  : " << smatch.str(4) << "\n";
+        // check if string a end tag
+        if (std::regex_search(tempSourceCode, match, REG::reg_endTagName)) {
+            // need to end element tag here when you switch elments vector to map 
+            elements[match.str(1)].hasEndTage = true;
+        }
+        else {
 
-      next++;
-  }*/
+            // get tag name
+            if (std::regex_search(tempSourceCode, match, REG::reg_tagName)) {
+                tempElement.setTagName(match.str(1));
+                full_ElementsPath += match.str(1);
+                full_ElementsPath += ".";
+            }
+
+            // get attrs & values
+            std::sregex_iterator itr_attr(tempSourceCode.begin(), tempSourceCode.end(), REG::reg_attr);
+            std::sregex_iterator itr_vals(tempSourceCode.begin(), tempSourceCode.end(), REG::reg_values);
+            std::sregex_iterator itr_end;
+
+            std::map<std::string, std::string> tempAttrs;
+            while (itr_attr != itr_end && itr_vals != itr_end) {
+                tempAttrs.insert({ itr_attr->str(1) , itr_vals->str(1) });
+                itr_attr++;
+                itr_vals++;
+            }
+            tempElement.setAttrs(tempAttrs);
+
+            // push to vector of HRML element  
+            elements.insert({ tempElement.getTagName() , tempElement });
+
+            // reffering to parent
+
+            for (std::map <std::string, HRML_ELEMENT>::iterator it = elements.begin(); it != elements.end(); it++) {
+                if (it->second.hasEndTage == false) {
+                    it->second.children.insert({ tempElement.getTagName() , &tempElement });
+                    break;
+                }
+            }
+
+        }
+    }
+
+
+
+
+    for (std::map<std::string, HRML_ELEMENT>::iterator element = elements.begin(); element != elements.end(); element++) {
+        std::cout << element->first << std::endl;
+
+        for (std::map<std::string, HRML_ELEMENT*>::iterator subelement = element->second.children.begin(); subelement != element->second.children.end(); subelement++) {
+            std::cout << "      " << subelement->first << std::endl;
+        }
+    }
+
+
+
+
+    /*
+        // processing lines "requestes"
+        for (int i = 0; i < lines; i += 1) {
+            std::string tempstr = arrlines[i] ; //std::getline(std::cin, tempstr);
+
+            // get path
+            std::string temppath = tempstr.substr(0, tempstr.find("~"));
+
+            // get attrbuit
+            std::string tempattr = tempstr.substr(tempstr.find("~") + 1, tempstr.size());
+
+            // check if "temp request path" in compatible with full_ElementsPath
+            if (full_ElementsPath.find(temppath) != std::string::npos) {
+
+                // get target element
+                std::string targetElementName;
+
+                //check if "temp path" has .  , that mean there's more than one element
+                if (temppath.find(".") != std::string::npos) {
+                    targetElementName = temppath.substr(temppath.rfind(".") + 1, temppath.size() - 1);
+                }
+                else { // if not that's mean "temp path" include only one element  ex => tag1~attr
+                    targetElementName = temppath;
+                }
+
+                // now after all that we need to access "target element" in "element map" by it's name
+                // and accessing request attrubites map inside "target element" by using "tempattr"
+
+                if (elements[targetElementName].attrs[tempattr] == "") {
+                    std::cout << "Not Found!" << std::endl;
+                }
+                else {
+                    std::cout << elements[targetElementName].attrs[tempattr] << std::endl;
+                }
+
+            }
+            else { // if not , that mean "temp request path" is an invalid path
+                std::cout << "Not Found!" << std::endl;
+            }
+        }
+        */
+/*
+    return 0;
+}
+*/
