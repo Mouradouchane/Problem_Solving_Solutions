77393468c16e7421e65ad6aa7417e52438f27562
Mourad ouchane <mouradouchane@gmail.com>
2021-05-31 07:28:57 +0100

    solving problem Class Template Specialization O(1)

--- ClassTemplateSpecialization.cpp
+++ ClassTemplateSpecialization.cpp
@@ -1,4 +1,4 @@
-
+#include <iostream>
 
 // problem https://www.hackerrank.com/challenges/cpp-class-template-specialization/problem
 // soultion O(1) =========================================================================
@@ -33,4 +33,36 @@ template <typename T> struct Traits {
         return "unknown"; // in case not found :)
     }
 };
+
+// another solution :)
+// solution O(1) =========================================================================
+
+#include<vector> // include vector for strick
+template <typename T> struct Traits {
+    static std::string name(int index) {
+        int fg = (int)Fruit::orange; // like first example for checking wich enum :)
+        int cg = (int)Color::orange; // like first example for checking wich enum :)
+        
+        // in this case we make vector take all values for O(1) access
+        std::vector<std::string> fruits = { "apple", "orange", "pear" }; 
+        std::vector<std::string> color = { "red", "green", "orange" };
+
+        // if fruit enum && index less than length of vector 
+        if ((int)T::orange == fg && index < fruits.size()) {
+            // just return fruit in index 
+            return fruits[index]; // O(1) :)
+        }
+
+        // same tricks like previous if 
+        if ((int)T::orange == cg && index < color.size()) {
+            // just return color in index 
+            return color[index]; // O(1) :)
+        }
+
+        // if no one we return unknown as last step :)
+        return "unknown";
+    }
+};
+
+
 // =======================================================================================
